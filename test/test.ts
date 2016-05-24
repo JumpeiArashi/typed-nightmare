5232a4ddf11734e811052f6b9d59bceebbdcb1a9
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2016-05-25 02:13:05 +0900

    goto google.com

--- test/test.ts
+++ test/test.ts
@@ -2,4 +2,4 @@ import test = require('blue-tape');
 
 import Nightmare = require('nightmare');
 
-new Nightmare({show: true}).goto('https://google.com').wait(5000).end().then(a => {a});
+new Nightmare({show: true}).goto('https://google.com').wait(5000).evaluate((hoge) => {}, 'st').end().then(a => {a});
